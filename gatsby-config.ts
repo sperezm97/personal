86f69da111be2da3d6b5378c6b21767a872a1c74
Sebastian Perez <sperezm97@gmail.com>
2022-06-09 00:59:00 -0400

    Add titlte with my name

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Personal`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `sperez`,
+    siteUrl: `https://www.sperez.dev`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
